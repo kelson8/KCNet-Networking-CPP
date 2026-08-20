4660918661d99280c2d8df91d5129a9791c87fc6
kelson8 <kelson@kelsoncraft.net>
2026-08-20 18:47:23 -0400

    feat: Move RSA encryption testing
    * I moved the RSA encryption testing out of openssl_test, and into rsa_test.

---
+++ TcpClient/src/util/rsa_test.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "defines.h"
+
+#ifdef __linux__
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+
+#endif // __linux__
+
+class RsaTest {
+
+public:
+    bool Encrypt(const unsigned char *plaintext,
+                 int plaintext_len,
+                 const unsigned char *key,
+                 const unsigned char *iv,
+                 unsigned char *ciphertext,
+                 int &ciphertext_len);
+
+    bool Decrypt(const unsigned char *ciphertext,
+                 int ciphertext_len,
+                 const unsigned char *key,
+                 const unsigned char *iv,
+                 unsigned char *plaintext,
+                 int &plaintext_len);
+
+    // void PublicKeyTesting();
+    std::string ReadPrivateKey(const char* privateKeyFile);
+    void TestRsa();
+
+    void NewRsaTest();
+
+private:
+#ifdef __linux__
+    EVP_PKEY &CreatePrivateKey();
+#endif // __linux__
+};
